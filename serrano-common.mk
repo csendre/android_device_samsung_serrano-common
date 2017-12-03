ba20b36fd75ad0b26b928f20d60fccd805c9fd6b
Endre <ecseko@gmail.com>
2017-12-03 18:42:10 -0500

    Revert "serrano-common: Remove GPS HAL"
    This reverts commit 6a81cab8450a5874718e7378a41b7ab03567c517.

--- serrano-common.mk
+++ serrano-common.mk
@@ -99,6 +99,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/libshims/libc.so:system/lib/libc.so
 
+# GPS HAL
+PRODUCT_PACKAGES += \
+    gps.msm8960 \
+    gps.conf \
+    sap.conf
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
