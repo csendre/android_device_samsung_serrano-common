fc46e7567abfa58616d2629ab96097ffccc157d2
Endre <ecseko@gmail.com>
2017-12-04 13:34:52 -0500

    Revert "serrano-common: Enable new libril"
    This reverts commit 8b6026c3576885fea86295ad280e16e09bd71bec.

--- system_prop.mk
+++ system_prop.mk
@@ -1,8 +1,9 @@
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.mobiledata=false \
-    persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1
+    ro.telephony.ril_class=SerranoRIL \
+    ro.ril.telephony.mqanelements=6 \
+    persist.radio.add_power_save=1
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
